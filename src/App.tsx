0c37c9b98ca17e6e7e1420eb1602930b80d74ec4
justBeroe <dobromirtt@gmail.com>
2026-02-22 00:46:38 +0200

    UX & Keyboard  - ScrollView and SafeAreaView, status reviewed.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import RootNavigator from "./navigation/RootNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "../src/core/services/AuthService";
-
+import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function App() {
   return (
+    <SafeAreaProvider>
     <AuthProvider>
     <NavigationContainer>
       <StatusBar style="auto" />
@@ -13,5 +14,6 @@ export default function App() {
      
     </NavigationContainer>
      </AuthProvider>
+     </SafeAreaProvider>
   );
 }
